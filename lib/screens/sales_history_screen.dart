690d9b35ff93aaba4f26fbc76ddef7a48d86805e
YunCheol1 <naro2727@naver.com>
2024-07-22 02:06:18 +0900

    no message

--- lib/screens/sales_history_screen.dart
+++ lib/screens/sales_history_screen.dart
@@ -1,56 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-// sale_item.dart
-class SaleItem {
-  final String title;
-  final String date;
-  final double price;
-  final String imageUrl;
+import 'package:provider/provider.dart';
+import '../providers/item_provider.dart';
+import 'item_detail_screen.dart';
 
-  SaleItem({
-    required this.title,
-    required this.date,
-    required this.price,
-  required this.imageUrl,
-  });
-}
-// sale_history_page.dart
 class SaleHistoryPage extends StatelessWidget {
-  final List<SaleItem> saleHistory = [
-    SaleItem(
-        title: '상품 1',
-        date: '2023-01-01',
-        price: 10000.0,
-        imageUrl: 'assets/images/charlie.png' // 예시 이미지 경로
-    ),
-    SaleItem(
-        title: '상품 2',
-        date: '2023-02-01',
-        price: 20000.0,
-        imageUrl: 'assets/images/tent.png'  // 예시 이미지 경로
-    ),
-    SaleItem(
-        title: '상품 3',
-        date: '2023-03-01',
-        price: 15000.0,
-        imageUrl: 'assets/images/tools.png'  // 예시 이미지 경로
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final itemProvider = Provider.of<ItemProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('판매내역'),
       ),
       body: ListView.builder(
-        itemCount: saleHistory.length,
+        itemCount: itemProvider.items.length,
         itemBuilder: (context, index) {
-          final item = saleHistory[index];
+          final item = itemProvider.items[index];
           return ListTile(
-            leading: Image.asset(item.imageUrl, width: 50, height: 50), // 이미지 추가
+            leading: item.itemImage != null
+                ? Image.memory(base64Decode(item.itemImage!), width: 50, height: 50)
+                : Placeholder(fallbackWidth: 50, fallbackHeight: 50),
             title: Text(item.title),
-            subtitle: Text('판매 날짜: ${item.date}'),
-            trailing: Text('₩${item.price.toStringAsFixed(0)}'),
+            subtitle: Text('판매 날짜: ${item.endDateTime.toLocal()}'.split(' ')[0]),
+            trailing: Text('₩${item.price}'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ItemDetailScreen(item: item),
+                ),
+              );
+            },
           );
         },
       ),
